9e1e0ed6fbc353a7b723701f5f6529e843cca99b
mar997799 <amramrajumadipalli@gmail.com>
2025-08-13 00:00:00 -0500

    Second commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media', // Enables system-based dark mode
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mar_portfolio/',        // <-- add this line
   plugins: [react()],
   assetsInclude: ['**/*.docx'],
 })
